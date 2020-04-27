03d95c28e233b784f86d7229ef15f37aac22e6e5
SayanAcharya <rowdysayan99@gmail.com>
2020-04-27 11:04:38 +0000

    synaptics_dsx_2.7: Report keycode for double tap as well
    *Fixes dt2w issues on synaptics panels
    *Use keycode 204 for both panels since most keycodes are problematic on synaptics
    
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
    Change-Id: I1d39329c523d123c1aafb5011654ecf5d91e15f9

--- drivers/input/touchscreen/NVT_Touch/nt36xxx.c
+++ drivers/input/touchscreen/NVT_Touch/nt36xxx.c
@@ -401,7 +401,8 @@ const uint16_t touch_key_array[TOUCH_KEY_NUM] = {
 #define GESTURE_EVENT_SWIPE_LEFT        257
 #define GESTURE_EVENT_SWIPE_RIGHT       258
 /* Huaqin modify gesture keycode by yuexinghan 20171109 start */
-#define GESTURE_EVENT_DOUBLE_CLICK KEY_WAKEUP
+/*#define GESTURE_EVENT_SWIPE_UP 248*/
+#define GESTURE_EVENT_DOUBLE_CLICK 204
 /* Huaqin modify gesture keycode by yuexinghan 20171109 end */
 
 const uint16_t gesture_key_array[] = {
